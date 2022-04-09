47f527f1877018d608a8d3ef606c7a46d01fd8b8
marcymarko <82332440+marcymarko@users.noreply.github.com>
2022-04-09 18:28:50 -0400

    Initial commit

---
+++ Models/Settings/MovieProSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace MoviePro.Models.Settings
+{
+    public class MovieProSettings
+    {
+        public string TmDbApikey { get; set; }
+        public string DefaultBackdropSize { get; set; }
+        public string DefaultPosterSize { get; set; }
+        public string DefaultYouTubeKey { get; set; }
+        public string DefaultCastImage { get; set; }
+        public DefaultCollection DefaultCollection { get; set; }
+        public DefaultCredentials DefaultCredentials { get; set; }
+    }
+
+    public class DefaultCollection
+    {
+        public string MyProperty { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class DefaultCredentials
+    {
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
